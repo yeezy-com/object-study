d95ac70bb7f5a6a6d925ad9c1ca40f9f1c06145c
GiSeok Kang <ime.rokz@gmail.com>
2024-10-31 01:44:57 +0900

    객체에게 자율성을 주자!
    - 개념적이나 물리적으로 객체 내부의 세부적인 사항을 감추는 것을 '캡슐화'
    - 캡슐화의 목적은 변경하기 쉬운 객체를 만드는 것
    - 객체를 인터페이스와 구현으로 나누고 인터페이스만을 공개하는 것은 객체 사이의 결합도를 낮추고 변경하기 쉬운 코드를 작성하기 위해 따라야 하는 가장 기본적인 설계 원칙
    - Audience와 TicketSeller가 내부 구현을 외부에 노출하지 않고 자율적인 존재가 되었다.

--- src/main/java/ch1_ticket/Audience.java
+++ src/main/java/ch1_ticket/Audience.java
@@ -8,7 +8,13 @@ public class Audience {
         this.bag = bag;
     }
 
-    public Bag getBag() {
-        return bag;
+    public Long buy(Ticket ticket) {
+        bag.setTicket(ticket);
+        if (bag.hasInvitation()) {
+            return 0L;
+        }
+
+        bag.minusAmount(ticket.getFee());
+        return ticket.getFee();
     }
 }
