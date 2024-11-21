124cf535623a575d62352cc25878ebc9d37f44a4
GiSeok Kang <ime.rokz@gmail.com>
2024-11-21 15:48:44 +0900

    협력하는 객체들의 공동체
    - 객체는 상태(state)와 행동(behavior)을 함께 가지는 복합적인 존재이다.
     - 객체는 스스로 판단하고 행동하는 자율적인 존재이다.
     - 캡슐화와 접근 제어로 객체는 두 부분으로 나뉨
      1. 퍼블릭 인터페이스(public interface): 외부에서 접근 가능한 부분
      2. 구현(Implementation): 내부에서만 접근 가능한 부분
     - 협력의 관점에서 먼저 어떤 객체가 필요한지를 결정하고, 객체들의 공통 상태와 행위를 구현하기 위해 클래스를 작성한다.

--- src/main/java/ch2_movie/Movie.java
+++ src/main/java/ch2_movie/Movie.java
@@ -1,4 +1,11 @@
 package ch2_movie;
 
 public class Movie {
+    public Money calculateMovieFee(Screening screening) {
+        return null;
+    }
+
+    public Money getFee() {
+        return null;
+    }
 }
